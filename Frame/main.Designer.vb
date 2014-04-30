8cb04c13acd4c429014500b39ac1104ab2a7fde8
wardwellb <ben@benwardwell.com>
2014-04-29 21:54:29 -0400

    Project written with simplicity in mind to transfer a random number of photos to a storage card.

---
+++ Frame/main.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBartransfer = New System.Windows.Forms.ProgressBar()
+        Me.ListBoxfiles = New System.Windows.Forms.ListBox()
+        Me.buttonlocation = New System.Windows.Forms.Button()
+        Me.butdestination = New System.Windows.Forms.Button()
+        Me.txtboxdestination = New System.Windows.Forms.TextBox()
+        Me.txtboxlocation = New System.Windows.Forms.TextBox()
+        Me.buttransfer = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Rbuttonsize = New System.Windows.Forms.RadioButton()
+        Me.rbuttonphotos = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Txtboxfill = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(572, 398)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBartransfer
+        '
+        Me.ProgressBartransfer.Location = New System.Drawing.Point(84, 416)
+        Me.ProgressBartransfer.Name = "ProgressBartransfer"
+        Me.ProgressBartransfer.Size = New System.Drawing.Size(444, 10)
+        Me.ProgressBartransfer.TabIndex = 1
+        '
+        'ListBoxfiles
+        '
+        Me.ListBoxfiles.FormattingEnabled = True
+        Me.ListBoxfiles.Location = New System.Drawing.Point(84, 276)
+        Me.ListBoxfiles.Name = "ListBoxfiles"
+        Me.ListBoxfiles.Size = New System.Drawing.Size(444, 134)
+        Me.ListBoxfiles.TabIndex = 2
+        '
+        'buttonlocation
+        '
+        Me.buttonlocation.Location = New System.Drawing.Point(470, 19)
+        Me.buttonlocation.Name = "buttonlocation"
+        Me.buttonlocation.Size = New System.Drawing.Size(77, 19)
+        Me.buttonlocation.TabIndex = 3
+        Me.buttonlocation.Text = "Browse"
+        Me.buttonlocation.UseVisualStyleBackColor = True
+        '
+        'butdestination
+        '
+        Me.butdestination.Location = New System.Drawing.Point(470, 44)
+        Me.butdestination.Name = "butdestination"
+        Me.butdestination.Size = New System.Drawing.Size(76, 19)
+        Me.butdestination.TabIndex = 4
+        Me.butdestination.Text = "Browse"
+        Me.butdestination.UseVisualStyleBackColor = True
+        '
+        'txtboxdestination
+        '
+        Me.txtboxdestination.Location = New System.Drawing.Point(122, 44)
+        Me.txtboxdestination.Name = "txtboxdestination"
+        Me.txtboxdestination.Size = New System.Drawing.Size(307, 20)
+        Me.txtboxdestination.TabIndex = 5
+        Me.txtboxdestination.Text = "Where to transfer the photos to"
+        '
+        'txtboxlocation
+        '
+        Me.txtboxlocation.Location = New System.Drawing.Point(122, 19)
+        Me.txtboxlocation.Name = "txtboxlocation"
+        Me.txtboxlocation.Size = New System.Drawing.Size(307, 20)
+        Me.txtboxlocation.TabIndex = 6
+        Me.txtboxlocation.Text = "Folder to transfer photos from"
+        '
+        'buttransfer
+        '
+        Me.buttransfer.Location = New System.Drawing.Point(572, 276)
+        Me.buttransfer.Name = "buttransfer"
+        Me.buttransfer.Size = New System.Drawing.Size(75, 20)
+        Me.buttransfer.TabIndex = 7
+        Me.buttransfer.Text = "Transfer"
+        Me.buttransfer.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(572, 211)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(73, 22)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(572, 182)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "size"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Rbuttonsize
+        '
+        Me.Rbuttonsize.AutoSize = True
+        Me.Rbuttonsize.Checked = True
+        Me.Rbuttonsize.Location = New System.Drawing.Point(362, 188)
+        Me.Rbuttonsize.Name = "Rbuttonsize"
+        Me.Rbuttonsize.Size = New System.Drawing.Size(45, 17)
+        Me.Rbuttonsize.TabIndex = 10
+        Me.Rbuttonsize.TabStop = True
+        Me.Rbuttonsize.Text = "Size"
+        Me.Rbuttonsize.UseVisualStyleBackColor = True
+        '
+        'rbuttonphotos
+        '
+        Me.rbuttonphotos.AutoSize = True
+        Me.rbuttonphotos.Location = New System.Drawing.Point(362, 216)
+        Me.rbuttonphotos.Name = "rbuttonphotos"
+        Me.rbuttonphotos.Size = New System.Drawing.Size(58, 17)
+        Me.rbuttonphotos.TabIndex = 11
+        Me.rbuttonphotos.Text = "Photos"
+        Me.rbuttonphotos.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(359, 162)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Fill Desination based on:"
+        '
+        'Txtboxfill
+        '
+        Me.Txtboxfill.Location = New System.Drawing.Point(442, 211)
+        Me.Txtboxfill.Name = "Txtboxfill"
+        Me.Txtboxfill.Size = New System.Drawing.Size(39, 20)
+        Me.Txtboxfill.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(703, 476)
+        Me.Controls.Add(Me.Txtboxfill)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.rbuttonphotos)
+        Me.Controls.Add(Me.Rbuttonsize)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.buttransfer)
+        Me.Controls.Add(Me.txtboxlocation)
+        Me.Controls.Add(Me.txtboxdestination)
+        Me.Controls.Add(Me.butdestination)
+        Me.Controls.Add(Me.buttonlocation)
+        Me.Controls.Add(Me.ListBoxfiles)
+        Me.Controls.Add(Me.ProgressBartransfer)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBartransfer As System.Windows.Forms.ProgressBar
+    Friend WithEvents ListBoxfiles As System.Windows.Forms.ListBox
+    Friend WithEvents buttonlocation As System.Windows.Forms.Button
+    Friend WithEvents butdestination As System.Windows.Forms.Button
+    Friend WithEvents txtboxdestination As System.Windows.Forms.TextBox
+    Friend WithEvents txtboxlocation As System.Windows.Forms.TextBox
+    Friend WithEvents buttransfer As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Rbuttonsize As System.Windows.Forms.RadioButton
+    Friend WithEvents rbuttonphotos As System.Windows.Forms.RadioButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Txtboxfill As System.Windows.Forms.TextBox
+
+End Class
---
+++ Frame/main.vb
@@ -0,0 +1,265 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim sdriveletter As String
+    Dim idrivesize As Integer
+    Dim filteredlist As New List(Of String)
+    Dim ltotransfer As New List(Of String)
+
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles buttonlocation.Click
+        ' First create a FolderBrowserDialog object
+        Dim FolderBrowserDialog1 As New FolderBrowserDialog
+
+        ' Then use the following code to create the Dialog window
+        ' Change the .SelectedPath property to the default location
+        With FolderBrowserDialog1
+            ' Desktop is the root folder in the dialog.
+            .RootFolder = Environment.SpecialFolder.Desktop
+            ' Select the C:\Windows directory on entry.
+            .SelectedPath = "c:\windows"
+            ' Prompt the user with a custom message.
+            .Description = "Select the source directory"
+            If .ShowDialog = DialogResult.OK Then
+                ' stores path in text box
+                txtboxlocation.Text = .SelectedPath
+            End If
+        End With
+    End Sub
+
+    Private Sub butdestination_Click(sender As System.Object, e As System.EventArgs) Handles butdestination.Click
+        ' First create a FolderBrowserDialog object
+        Dim FolderBrowserDialog2 As New FolderBrowserDialog
+
+        ' Then use the following code to create the Dialog window
+        ' Change the .SelectedPath property to the default location
+        With FolderBrowserDialog2
+            ' Desktop is the root folder in the dialog.
+            .RootFolder = Environment.SpecialFolder.Desktop
+            ' Select the C:\Windows directory on entry.
+            .SelectedPath = "c:\"
+            ' Prompt the user with a custom message.
+            .Description = "Select the source directory"
+            If .ShowDialog = DialogResult.OK Then
+                ' stores path in text box
+                txtboxdestination.Text = .SelectedPath
+            End If
+        End With
+    End Sub
+
+    Private Sub Button2_Click_1(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Call cleardisk(txtboxdestination.Text)
+
+    End Sub
+    Public Sub Getfiles()
+        ' Get recursive List of all files starting in this directory.
+        Dim list As List(Of String) = FileHelper.GetFilesRecursive(txtboxlocation.Text)
+
+        Dim ispaceleft As Integer = idrivesize
+        'Dim ifilteredlistcount As Integer = 0
+
+        'Dim ifilecount As Integer = 0
+
+        FileHelper.GetFilesRecursive(txtboxlocation.Text)
+        ' Loop through and display each path.
+        For Each photo In list
+            'ListBoxfiles.Items.Add("Test")
+            If photo.Trim().EndsWith(".jpg") Or photo.Trim().EndsWith(".JPG") Or photo.Trim().EndsWith(".jpeg") Then
+                'ifilecount = ifilecount + 1
+                'ListBoxfiles.Items.Add((My.Computer.FileSystem.GetFileInfo(path).Length / 1048576) & " MB")
+                'ListBoxfiles.Items.Add(path.Remove(0, (txtboxlocation.TextLength + 1)))
+                ' Put files into Dictionary Of String.
+
+                'filteredlist.Add(path.Remove(0, (txtboxlocation.TextLength + 1)))
+                filteredlist.Add(photo)
+
+            End If
+            'ListBoxfiles.Items.Add(path.ToString())
+            'Console.WriteLine(path)
+        Next
+
+        Dim irandomint As Integer = 0
+
+
+
+        If Rbuttonsize.Checked Then
+            Dim ifillsize As Integer = idrivesize - (idrivesize * (Txtboxfill.Text / 100))
+            Dim icount As Integer = filteredlist.Count
+            'MsgBox("Size")
+            While (ispaceleft > ifillsize And filteredlist.Count >= 1)
+
+                'ispaceleft = ispaceleft - My.Computer.FileSystem.GetFileInfo(txtboxlocation.Text & "\" & filteredlist.Item(5).ToString).Length / 1048576
+                'icount = icount - 1
+                irandomint = randomphoto()
+                ispaceleft = ispaceleft - My.Computer.FileSystem.GetFileInfo(filteredlist.Item(irandomint).ToString).Length / 1048576
+                'ListBoxfiles.Items.Add("Random Number:" & irandomint)
+                'ListBoxfiles.Items.Add(ispaceleft & " MB")
+                'ListBoxfiles.Items.Add(filteredlist.Item(icount).ToString)
+                ltotransfer.Add(filteredlist.Item(irandomint).ToString)
+                filteredlist.RemoveAt(irandomint)
+                If filteredlist.Count = 0 Then
+                    MsgBox("There was not enough photos to fill the card")
+                End If
+            End While
+        End If
+
+        If rbuttonphotos.Checked Then
+            Dim ifillamount As Integer = filteredlist.Count - Txtboxfill.Text
+            Dim icount As Integer = filteredlist.Count
+            irandomint = randomphoto()
+            'MsgBox("Photos")
+            While (filteredlist.Count > ifillamount And filteredlist.Count >= 1 And ispaceleft > idrivesize)
+                'ispaceleft = ispaceleft - My.Computer.FileSystem.GetFileInfo(txtboxlocation.Text & "\" & filteredlist.Item(5).ToString).Length / 1048576
+                'ListBoxfiles.Items.Add(ispaceleft & " MB")
+                'icount = icount - 1
+                'ListBoxfiles.Items.Add(filteredlist.Count)
+                'ListBoxfiles.Items.Add(filteredlist.Item(icount).ToString)
+                'ListBoxfiles.Items.Add("Random Number:" & irandomint)
+                While (irandomint.Equals(filteredlist.Count))
+                    irandomint = randomphoto()
+                End While
+                ispaceleft = ispaceleft - My.Computer.FileSystem.GetFileInfo(filteredlist.Item(irandomint).ToString).Length / 1048576
+                ltotransfer.Add(filteredlist.Item(irandomint).ToString)
+                filteredlist.RemoveAt(irandomint)
+                If filteredlist.Count = 0 Then
+                    MsgBox("There was not enough photos to fill the card")
+                End If
+            End While
+        End If
+
+
+        For displayfilteredlist = 1 To (ltotransfer.Count)
+            'Displays whats in the list.
+            'ispaceleft = ispaceleft - My.Computer.FileSystem.GetFileInfo(txtboxlocation.Text & "\" & filteredlist.Item(displayfilteredlist - 1).ToString).Length / 1048576
+            'ListBoxfiles.Items.Add(ispaceleft & " MB")
+            ListBoxfiles.Items.Add(ltotransfer.Item(displayfilteredlist - 1).Remove(0, (txtboxlocation.TextLength + 1)).ToString)
+            ListBoxfiles.Update()
+        Next displayfilteredlist
+
+        'Write total number of paths found.
+        'ListBoxfiles.Items.Add(list.Count)
+        Return
+    End Sub
+
+    Public Sub Getfreespace()
+        'Dim oDinfo As DriveInfo
+        Dim oDrvs() As DriveInfo = DriveInfo.GetDrives
+
+        For Each Drv In oDrvs
+            'MsgBox(txtboxdestination.Text.Remove(3))
+            If txtboxdestination.Text.Length > 3 Then
+                sdriveletter = txtboxdestination.Text.Remove(3)
+            Else
+                sdriveletter = txtboxdestination.Text
+            End If
+
+            If Drv.IsReady And Drv.Name = sdriveletter Then
+                idrivesize = Math.Round(Drv.AvailableFreeSpace.ToString / 1048576) - 25
+                'MsgBox(Drv.Name & " " & idrivesize & " MB")
+            End If
+        Next
+        Return
+    End Sub
+
+
+    Function randomphoto()
+        Dim MinNumber As Integer = 0
+        Dim randomnumber As Integer
+
+        'initialize random number generator
+        Dim r As New Random(System.DateTime.Now.Millisecond)
+
+        randomnumber = r.Next(MinNumber, filteredlist.Count)
+
+        While (filteredlist.Count = randomnumber)
+            'MsgBox("FAIL")
+            randomnumber = r.Next(MinNumber, filteredlist.Count)
+        End While
+
+        Return randomnumber
+
+    End Function
+    Public Sub Transferfiles()
+        Dim sfiletocopyfrom As String
+        Dim sfiletocopyto As String
+        For displayfilteredlist = 0 To (ltotransfer.Count - 1)
+            sfiletocopyfrom = ltotransfer.Item(displayfilteredlist).ToString
+            sfiletocopyto = txtboxdestination.Text.ToString & getfilename(ltotransfer.Item(displayfilteredlist)).ToString
+            File.Copy(sfiletocopyfrom, sfiletocopyto)
+            ProgressBartransfer.Value = (displayfilteredlist / (ltotransfer.Count)) * 100
+        Next displayfilteredlist
+
+    End Sub
+
+    Public Function getfilename(ByVal sFilelocation As String)
+        Dim sfilename As String
+        Dim split As String() = sFilelocation.Split("\")
+        For Each s As String In split
+            If s.Trim.Contains(".jpg") Or s.Trim.Contains(".JPG") Or s.Trim.Contains(".jpeg") Then
+                sfilename = s
+                'MsgBox(sfilename)
+            End If
+        Next s
+
+        Return sfilename
+    End Function
+
+
+    Public Function cleardisk(ByVal slocation As String)
+        Dim file As String
+        ' Initializes variables to pass to the MessageBox.Show method.
+
+        Dim Message As String = "Would you like to delete all photos on the card?"
+        Dim Caption As String = "Delete all photos on card"
+        Dim Buttons As MessageBoxButtons = MessageBoxButtons.YesNo
+
+        Dim Result As DialogResult
+
+        'Displays the MessageBox
+
+        Result = MessageBox.Show(Message, Caption, Buttons)
+
+        ' Gets the result of the MessageBox display.
+
+        If Result = System.Windows.Forms.DialogResult.Yes Then
+
+            ' Deletes the Files.
+            For Each file In System.IO.Directory.GetFiles(slocation)
+                System.IO.File.Delete(file)
+            Next file
+
+        End If
+
+    End Function
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Call Getfreespace()
+    End Sub
+
+    Private Sub buttransfer_Click(sender As System.Object, e As System.EventArgs) Handles buttransfer.Click
+        If Rbuttonsize.Checked And Txtboxfill.Text > 0 And Txtboxfill.Text <= 100 Or rbuttonphotos.Checked Then
+            ListBoxfiles.Items.Clear()
+            ltotransfer.Clear()
+            filteredlist.Clear()
+            Call Getfreespace()
+            'ListBoxfiles.Items.Add(idrivesize)
+            Call Getfiles()
+            Call Transferfiles()
+        Else
+            MsgBox("Please enter a value between 1 and 100 %")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        txtboxlocation.Text = "C:\Documents and Settings\bwardwell\My Documents\My Pictures\Wallpapers"
+        txtboxdestination.Text = "E:\"
+        Txtboxfill.Text = "100"
+    End Sub
+
+End Class
